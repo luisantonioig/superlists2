c968f4df110ce5d718e49f2f1fcfd2ee5a2f2186
luisantonioig <c-remix@hotmail.es>
2017-11-08 10:06:08 -0600

    Refractor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -14,7 +14,11 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = str(response.content).decode('utf8')
+        html = str(response.content).encode("utf-8").replace('\n', '')
         self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertIn("<title>To-Do lists</title>".encode("utf-8"), html)
+        self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')

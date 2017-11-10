205ec82763e67403ee2f52074cc9d31e03a15a77
luisantonioig <c-remix@hotmail.es>
2017-11-10 11:51:54 -0600

    Recfactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -69,14 +69,12 @@ class ListViewTest(TestCase):
         self.asserEqual(List.objects.count(), 0)
         self.asserEqual(Item.objects.count(), 0)
 
-class NewListTest(TestCase):
-
     def test_can_save_a_POST_request(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-            '/lists/'+str(correct_list.id)+'/add_item',
+            '/lists/'+str(correct_list.id)+'/',
             data={'item_text': 'A new item for an existing list'}
         )
 
@@ -94,4 +92,13 @@ class NewListTest(TestCase):
             '/lists/' + str(correct_list.id) + '/add_item',
             data={'item_text': 'A new item for an existing list'})
 
+        self.assertRedirects(response, '/lists/'+str(correct_list.id)+'/')
+
+    def test_POST_redirects_to_live_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        self.client.post(
+            '/lists/'+str(correct_list.id)+'/',
+            data={'item_text': 'A new item for an existing list'}
+        )
         self.assertRedirects(response, '/lists/'+str(correct_list.id)+'/')
